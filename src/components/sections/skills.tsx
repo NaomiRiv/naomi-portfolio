61ec96a259808415a65c06b38cc1858c8d7cfcf6
Naomi Rivkin <nomi420@gmail.com>
2025-06-17 15:48:21 +0300

    Add skills list

--- src/components/sections/skills.tsx
+++ src/components/sections/skills.tsx
@@ -1,3 +1,40 @@
 export function Skills() {
+  const skillCategories = [
+    {
+      title: "Programming Languages",
+      icon: "💻",
+      skills: ["C", "C++", "Python", "R", "JavaScript", "SQL", "Bash"],
+    },
+    {
+      title: "Systems & Tools",
+      icon: "🖥️",
+      skills: ["Linux", "Docker", "Git", "HPC/Cluster Computing", "IPC"],
+    },
+    {
+      title: "Concepts & Methodologies",
+      icon: "📚",
+      skills: [
+        "Object-Oriented Programming",
+        "Multithreading",
+        "CI/CD",
+        "Data Analysis",
+      ],
+    },
+    {
+      title: "Other",
+      icon: "🌐",
+      skills: [
+        "Pandas",
+        "PyTorch",
+        "TensorFlow",
+        "NumPy",
+        "OpenCV",
+        "HTML",
+        "CSS",
+        "React",
+      ],
+    },
+  ];
+
   return <section id="skills">skills</section>;
 }
